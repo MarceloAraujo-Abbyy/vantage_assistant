7f8061feefd6733ac8b8fbcc0aca3aa3269a9f68
MarceloAraujo-Abbyy <marcelo.araujo@abbyy.com>
2024-06-21 09:29:51 -0300

    modify login
    Show tenant name instead of tenant id

--- README.md
+++ README.md
@@ -1,23 +1,18 @@
 # Vantage Assistant
 
-This application uses RAG (Retrieval Augmented Generation) and LLM (ChatGPT OpenAI) to access all Vantage documentation, and let you interact with the manuals using natural language. 
+This application uses RAG (Retrieval Augmented Generation) and LLM (ChatGPT OpenAI) to access all Vantage documentation, and let you interact with the Vantage manuals using natural language. 
 
-
-Install dependencies.
-
-```python
-pip install -r requirements.txt
-```
 
 ## Usage
 
-You'll need to set up your ABBYY Vantage tenant abd OpenAI account. 
+You'll need to set up your ABBYY Vantage tenant and OpenAI account. 
 
 When create your streamlit app, set the secrets with the according information: 
 
 ```python
 OPENAI_API_KEY = ""
 VANTAGE_CLIENT_ID = "" 
-VANTAGE_SECRET_ID = ""
+VANTAGE_CLIENT_SECRET = ""
 VANTAGE_TENANT_ID = "" 
+VANTAGE_TENANT_NAME = ""
 
--- vantage_assistant.py
+++ vantage_assistant.py
@@ -2,16 +2,12 @@ __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-
 import sqlite3
 import streamlit as st
 import random
 import time
 import os
 from query_data import get_answer
-
-
-###  streamlit run C:\Users\marceloraraujo\Documents\vantage_assistant\vantage_assistant\vantage_assistant.py
 
 # Function to get the content from a BaseMessage object
 def get_message_content(message) -> str:
@@ -32,7 +28,6 @@ def get_message_source(message) -> str:
 # Streamed response emulator
 def response_generator(prompt):
     response, source = get_answer(prompt)
-    print("returned: ", response, source)
     resp = get_message_content(response)
     sour = "Source: " + source[0]
     for word in str(resp).split():
@@ -44,47 +39,49 @@ def response_generator(prompt):
 
 
 # Streamed response emulator
-def login_vantage():
-    print("Login Vantage")
+def login_vantage(username,password):
+    tenant_id = st.secrets["VANTAGE_TENANT_ID"]
+    client_id = st.secrets["VANTAGE_CLIENT_ID"]
+    client_secret = st.secrets["VANTAGE_CLIENT_SECRET"]
     if username != "" and password != "":
         import requests
         import json
-        url = "https://vantage-us.abbyy.com/auth2/"+tenant+"/connect/token"
-        payload = 'grant_type=password&scope=openid permissions global.wildcard&username='+username+'&password='+password+'&client_id='+client_id+'&client_secret='+secret_id
+        url = "https://vantage-us.abbyy.com/auth2/"+tenant_id+"/connect/token"
+        payload = 'grant_type=password&scope=openid permissions global.wildcard&username='+username+'&password='+password+'&client_id='+client_id+'&client_secret='+client_secret
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
         response = requests.request("POST", url, headers=headers, data=payload)
         obj = json.loads(response.text)
-        print(obj)
         if "access_token" in obj:
             accessToken = "Bearer " + str(obj["access_token"])
             st.session_state['token'] = accessToken
-            st.markdown("Logged with Success!")
+            st.session_state['status'] = "🟢 Logged in " + tenant_name
         else:
             st.session_state['token'] = ''
-            st.markdown("Error to login! ")
+            st.session_state['status'] = "🔴 Error to logging in " + tenant_name
 
 if 'token' not in st.session_state:
     st.session_state['token'] = ''
+if 'status' not in st.session_state:
+    st.session_state['status'] = "🟠 Disconnected "
+
+st.title("ABBYY Vantage Assistant")
+st.write("Author: marcelo.araujo@abbyy.com")
 
 with st.sidebar:
-    tenant = st.secrets["VANTAGE_TENANT_ID"]
-    client_id = st.secrets["VANTAGE_CLIENT_ID"]
-    secret_id = st.secrets["VANTAGE_SECRET_ID"]
+    tenant_name = st.secrets["VANTAGE_TENANT_NAME"]
     st.image("abbyy.png")
     st.title("Vantage Login")
-    tenant = st.text_input("Tenant", tenant ,disabled=True)
+    tenant_name = st.text_input("Tenant", tenant_name ,disabled=True)
     username = st.text_input("Email")
     password = st.text_input("Password",type="password")
-    st.button("Login", on_click=login_vantage())
+    st.button("Login", on_click=login_vantage,args=[username,password])
+    status = st.text_input("Status", value=st.session_state['status'], disabled=True)
 
 if  st.session_state["token"] != "":
 
     # Initialize chat history
     if "messages" not in st.session_state:
         st.session_state.messages = []
-
-    st.title("ABBYY Vantage Assistant")
-    st.write("Author: marcelo.araujo@abbyy.com")
 
     # Display chat messages from history on app rerun
     for message in st.session_state.messages:
